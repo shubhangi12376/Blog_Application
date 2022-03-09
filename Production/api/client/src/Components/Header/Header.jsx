ce1bfd9527b269b31e70762976e3caa4f660f3c7
Shubhangi Barvekar <shubhu178@gmail.com>
2022-03-09 19:54:22 +0530

    initial commit

---
+++ Production/api/client/src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className='headerTitles'> 
+                <span className='headerTitlelg'>BLOG</span>
+            </div>
+            <img className='headerImg'  alt='' />
+        </div>
+    )
+}
+
+export default Header;
---
+++ Production/api/index.js
@@ -0,0 +1,67 @@
+const express = require("express")
+const path = require("path");
+const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config({path: './.env'});
+
+app.use(express.urlencoded())
+const mongoose = require("mongoose");
+const authRoute = require('./Routes/auth');
+const userRoute = require('./Routes/users');
+const postRoute = require('./Routes/posts')
+const catRoute = require('./Routes/categories')
+const multer = require("multer");
+const { error } = require("console");
+
+app.use(express.json())
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+mongoose.connect("mongodb://localhost:27017/BlogUsers" , {
+
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    
+}).then(console.log("DB connected"))
+  .catch((err) => console.log(err));
+
+
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
+  
+  const upload = multer({ storage : storage });
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    try{
+      return res.status(200).json("File has been uploaded");
+    }
+    catch(error){
+      console.error(error);
+    }
+  });
+  
+  
+
+
+app.use("/api/auth" , authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts" , postRoute);
+app.use('/api/categories' , catRoute);
+
+
+const PORT = process.env.PORT || 9002;
+
+
+if(process.env.NODE_ENV=="production"){
+  app.use(express.static("client/build"));
+}
+
+
+  app.listen(PORT , () => {
+    console.log("backend started")
+})
